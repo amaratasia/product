a4c4f033a733093c2855a163c6b14baa2949c793
Amar A <amaratasia@gmail.com>
2017-02-27 20:02:04 +0530

    Category Create/Delete

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,22 +16,26 @@ class Product < ActiveRecord::Base
 
   def tags_attributes=(tags_attributes)
     tags_attributes.each do |tag_attributes|
-      if (tag_attributes["_destroy"] == "1") 
-        self.tags.delete(tag_attributes["id"])
-      else
-        tag_attributes.delete("_destroy")
-        new_tag = Tag.find_or_create_by(:name =>tag_attributes[:name])
-        self.tags << new_tag unless new_tag.in? self.tags
+      if tag_attributes["name"]
+        if (tag_attributes["_destroy"] == "1")
+          self.tags.delete(tag_attributes["id"])
+        else
+          tag_attributes.delete("_destroy")
+          new_tag = Tag.find_or_create_by(:name =>tag_attributes[:name])
+          self.tags << new_tag unless new_tag.in? self.tags
+        end
       end
     end
   end
   def categories_attributes=(categories_attributes)
     categories_attributes.each do |category_attributes|
-      if (category_attributes["_destroy"] == "1") 
-        self.categories.delete(category_attributes["id"])
-      else
-        new_category = Category.find_or_create_by(:name =>category_attributes[:name])
-        self.categories << new_category unless new_category.in? self.categories
+      if category_attributes["name"]
+        if (category_attributes["_destroy"] == "1") 
+          self.categories.delete(category_attributes["id"])
+        else
+          new_category = Category.find_or_create_by(:name =>category_attributes[:name])
+          self.categories << new_category unless new_category.in? self.categories
+        end
       end
     end
   end
