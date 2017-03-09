91f4330b834d3a1965de60ff00acb63519e20348
Amar A <amaratasia@gmail.com>
2017-03-09 19:05:20 +0530

    Tags Cloud Feature

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,6 @@
 # Controller for all products
 class Api::V1::ProductsController < ApplicationController
+  before_action :get_product, only: [:edit, :update, :show]
   def create
     begin
       product = Product.create(product_params)
@@ -14,14 +15,17 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
   def index
-    @products = Product.joins("left join images on images.product_id = products.id").select("products.*, images.img_path img").group("products.id")
+    @products = Product.joins("left join images on images.product_id = products.id").select("products.*, images.img_path img")
+    if params["tag_name"].present?
+      @products = @products.joins(:tags).where("tags.name = ?",params["tag_name"])
+    end
+    @products = @products.group("products.id")
+    @tags = Tag.joins(:products).select("tags.name, count(tags.name) tag_count").group("tags.name")
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update_attributes(product_params)
       flash[:success] = "Product Updated"
     else 
@@ -42,5 +46,9 @@ class Api::V1::ProductsController < ApplicationController
     return params.require(:product).permit(:name, :sku_id, :price, :description, :expire_date, images_attributes: [:img_path], tags_attributes: [:id, :name, :_destroy], categories_attributes: [:id, :name, :_destroy]) if (params[:action] == "create")
 
     return params.require(:product).permit(:name, :sku_id, :price, :description, :expire_date, :admin_approved, images_attributes: [:img_path], tags_attributes: [:id, :name, :_destroy], categories_attributes: [:id, :name, :_destroy])
+  end
+
+  def get_product
+    @product = Product.find_by_id(params[:id])
   end
 end
